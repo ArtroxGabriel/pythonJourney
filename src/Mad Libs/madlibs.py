e4d3a87b23eb1f4fdf909ef414773287fc0016e1
ArtroxGabriel <artroxgabriel@gmail.com>
2023-10-23 19:58:22 -0300

    git ignore + madlibs

--- src/Mad Libs/madlibs.py
+++ src/Mad Libs/madlibs.py
@@ -2,8 +2,11 @@
 # Concatenação de strings, e.g, "Hello, " + "world!" = "Hello, world!
 # Primeiro projeto extremamento basico que funcionara como pseudo hello world :)
 def execute():
-    standard = "subscribe to "
-    phrase = input("Enter a youtube channel to subscribe to: ")
-    print(standard + phrase)                 # Concatenação
-    print("Subscribe to {}".format(phrase))  # .format() 
-    print(f"Subscribe to {phrase}")          # f-strings
+    phrase = input("Enter a phrase: ")
+    standard = f"subscribe to {phrase}"
+    #print(standard + phrase)                 # Concatenação
+    #print("Subscribe to {}".format(phrase))  # .format() 
+    #print(f"Subscribe to {phrase}")          # f-strings
+
+    
+    print(standard)
