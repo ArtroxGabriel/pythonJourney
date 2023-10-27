c85ed23741b425ed8f15146a485aed8560f04e88
ArtroxGabriel <artroxgabriel@gmail.com>
2023-10-27 14:27:30 -0300

    gerador de senha

--- src/codecamp/passwordGen/pass_gen.py
+++ src/codecamp/passwordGen/pass_gen.py
@@ -9,11 +9,26 @@ def generator():
     ||                    ||
     ++====================++
 """)
-    quant = int(input("Quantas senhas voce deseja gerar?"))
-    tam = int(input("Ensira o tamanho das senhas"))
+    quant = int(input("Quantas senhas voce deseja gerar? "))
+    tam = int(input("Insira o tamanho das senhas: "))
 
-    chars = string.printable
-    print(chars)
+    chars = string.ascii_lowercase + string.digits
+
+    escolhas = input("\nSua senha deve ter letra maiuscula?\n Digite 's' para confirmar ")
+    if escolhas.lower() == 's':
+        chars += string.ascii_uppercase
+
+    escolhas = input("\nSua senha deve ter caracteres especiais??\n Digite 's' para confirmar ")
+    if escolhas.lower() == 's':
+        chars += string.punctuation
+
+    print(f"\n Sua senha de {tam} digitos está aqui:")
+    for pwd in range(quant):
+        passwords = ''
+
+        for c in range(tam):
+            passwords += random.choice(chars)
+        print(f"\t{passwords}\n")
 
 
 
