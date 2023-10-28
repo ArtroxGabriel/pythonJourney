562206e5f96e752181fdc8bba2450db153fe61c8
ArtroxGabriel <artroxgabriel@gmail.com>
2023-10-28 13:14:13 -0300

    formatando

--- src/codecamp/tictactoe/player.py
+++ src/codecamp/tictactoe/player.py
@@ -1,12 +1,14 @@
 import random
 import time
 
+
 class Player:
     def __init__(self, letter):
         self.letter = letter
 
     def get_move(self, game):
         pass
+
 
 class RandomComputerPlayer(Player):
     def __init__(self, letter):
@@ -17,6 +19,7 @@ class RandomComputerPlayer(Player):
         time.sleep(0.25)
         return square
 
+
 class HumanPlayer(Player):
     def __init__(self, letter):
         super().__init__(letter)
@@ -26,7 +29,7 @@ class HumanPlayer(Player):
         val = None
 
         while not valid_square:
-            square = input(self.letter + "\'s turn. Input move (0-9):")
+            square = input(self.letter + "'s turn. Input move (0-9):")
             try:
                 val = int(square)
                 if val not in game.available_moves():
